a8bc2612868e6a0d0dd013f869afead31611a3c8
NotoriousArnav <arnavghosh.official.2004@gmail.com>
2026-05-28 21:44:39 +0530

    Initial Commit.
    This Commit Marks the Starting of this Repo

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple ExpressJS and TypeScript Application
+
+Made during tutorial
+
+## Usage
+To run dev server, run the following commands in the terminal:
+```bash
+npm i
+npm run dev
+```
+```
+```
---
+++ index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  const data = {
+    name: 'Trisha'
+  }
+  res.json(data);
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+})
